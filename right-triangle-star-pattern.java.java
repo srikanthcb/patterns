89ce8809ff24df1ac5ad3e7bf87a3d00e05c9def
srikanthcb <110121654+srikanthcb@users.noreply.github.com>
2023-10-31 10:11:36 +0530

    Add files via upload

---
+++ right-triangle-star-pattern.java.java
@@ -0,0 +1,15 @@
+public class Main   
+{   
+public static void main(String args[])   
+{   
+int i, j, row=4;   
+for(i=0; i<row; i++)   
+{   
+for(j=0; j<=i; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}   
+}   
+}  
